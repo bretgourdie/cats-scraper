f1f84e827553bbe0f688fb0e02169601b38753c1
Bret Gourdie <BretGourdie@gmail.com>
2018-02-24 01:00:03 -0500

    Completed prototype

--- web-scrape.py
+++ web-scrape.py
@@ -1,22 +1,63 @@
 import requests
 
-def processLines(lines, year):
+def catchLines(lines, target, linesToGrab):
     splitLines = lines.splitlines()
-    target = "16.5"
     foundTarget = False
+    caughtLines = []
 
     for line in splitLines:
         if target in line:
             foundTarget = True
-            print("{}: {}".format(year, line))
 
-    if not foundTarget:
+        if foundTarget:
+            if linesToGrab > 0:
+                caughtLines.append(line)
+                linesToGrab -= 1
+            else:
+                break
+
+    return caughtLines
+
+def sanitizeLines(caughtLines):
+    textToStrip = [
+        "<td valign=\"top\" class=\"rpt127\">",
+        "</td>",
+        "<td valign=\"top\" class=\"rpt127r\">"
+    ]
+
+    sanitizedLine = ""
+
+    for line in caughtLines:
+        strippedLine = line.strip()
+
+        clearedLine = strippedLine
+
+        for oneTextToStrip in textToStrip:
+            clearedLine = clearedLine.replace(oneTextToStrip, "")
+
+        if clearedLine != "":
+            sanitizedLine += " " + clearedLine
+
+    return sanitizedLine
+
+
+def processLines(lines, year):
+    target = "16.5"
+    linesToGrab = 5
+    
+    caughtLines = catchLines(lines, target, linesToGrab)
+    if len(caughtLines) > 0:
+        sanitizedLine = sanitizeLines(caughtLines)
+        print("{}: {}".format(year, sanitizedLine))
+    else:
         print("{}: Did not find {}".format(year, target))
+
 
 baseUrl = "https://cats.airports.faa.gov/Reports/reports.cfm"
 startYear = 2009
 endYear = 2017
 pitAirportId = 2179
+debug = False
 
 for currentYear in range(startYear, endYear + 1):
     postRequestUrl = "https://cats.airports.faa.gov/Reports/rpt127.cfm"
@@ -31,7 +72,8 @@ for currentYear in range(startYear, endYear + 1):
         "YearToCompare": "0"
     }
 
-    print("Posting \"{}\"".format(postRequestUrl))
+    if debug:
+        print("DEBUG: Posting \"{}\" with payload \"{}\"".format(postRequestUrl, payload))
 
     session = requests.Session()
     r = session.post(postRequestUrl, headers=headers, data=payload)
