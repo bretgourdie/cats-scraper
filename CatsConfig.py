aa9f75637a1159a17c91e82c2cecdcc1ee66a8f2
Bret Gourdie <BretGourdie@gmail.com>
2018-02-24 01:23:13 -0500

    Added config file and parsing class

---
+++ CatsConfig.py
@@ -0,0 +1,20 @@
+import ConfigParser
+
+class CatsConfig:
+
+    def __init__(self, section="DEFAULT"):
+        configFileName = "cats-scraper.ini"
+
+        config = configparser.ConfigParser()
+        config.read(configFileName)
+
+        targetSection = config[section]
+
+        self.ReportUrl = targetSection["ReportUrl"]
+        self.StartYear = int(targetSection["StartYear"])
+        self.EndYear = int(targetSection["EndYear"])
+        self.AirportId = targetSection["AirportId"]
+        self.State = int(targetSection["State"])
+        self.RegionId = int(targetSection["RegionId"])
+        self.view = targetSection["view"]
+        self.YearToCompare = int(targetSection["YearToCompare"])
